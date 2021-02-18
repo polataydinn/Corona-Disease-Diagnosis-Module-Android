4d9af956a961ac17e8f88a059b405c44470c00db
polat <aydinpolat@post.com>
2021-02-18 21:19:09 +0300

    done for now

---
+++ src/main/java/com/example/corona_disease_module/CONST.java
@@ -0,0 +1,26 @@
+package com.example.corona_disease_module;
+
+public abstract class CONST {
+    public static float hastalikTaniOran = 0;
+    public static int atesResult;
+    public static int yorgunlukResult;
+    public static int kuruOksurukResult;
+    public static int solunuResult;
+    public static int oksurukResult;
+    public static int agriResult;
+    public static int hapsirmaResult;
+    public static int burunAkintiResult;
+    public static int burunTikanikResult;
+    public static int sulanmaResult;
+    public static int bogazAgriResult;
+    public static int ishalResult;
+    public static float sogukAlginligiYuzde;
+    public static float gripYuzde;
+    public static float alerjiYuzde;
+    public static float koronaYuzde;
+    public static boolean koronaBool;
+    public static boolean sogukAlginligiBool;
+    public static boolean gripBool;
+    public static boolean alerjiBool;
+}
+
---
+++ src/main/java/com/example/corona_disease_module/DiseaseRate.java
@@ -0,0 +1,170 @@
+package com.example.corona_disease_module;
+
+public class DiseaseRate {
+    public static void yokCevap(int deger){
+        if(deger == 0){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 100;
+        }
+        if(deger == 1){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 0;
+        }
+        if(deger == 2){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 0;
+        }
+        if(deger == 3){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 0;
+        }
+        if(deger == 4){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 0;
+        }
+    }
+
+    public static void nadirenCevap(int deger){
+        if(deger == 0){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 50;
+        }
+        if(deger == 1){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 100;
+        }
+        if(deger == 2){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 50;
+        }
+        if(deger == 3){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 25;
+        }
+        if(deger == 4){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 0;
+        }
+    }
+
+    public static void bazenCevap(int deger){
+        if(deger == 0){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 25;
+        }
+        if(deger == 1){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 50;
+        }
+        if(deger == 2){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 100;
+        }
+        if(deger == 3){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 50;
+        }
+        if(deger == 4){
+            CONST.hastalikTaniOran = CONST.hastalikTaniOran + 25;
+        }
+    }
+
+    public static void sikCevap(int deger){
+        if(deger == 0){
+            CONST.hastalikTaniOran = (CONST.hastalikTaniOran + 0);
+        }
+        if(deger == 1){
+            CONST.hastalikTaniOran = (CONST.hastalikTaniOran + 25);
+        }
+        if(deger == 2){
+            CONST.hastalikTaniOran = (CONST.hastalikTaniOran + 50);
+        }
+        if(deger == 3){
+            CONST.hastalikTaniOran = (CONST.hastalikTaniOran + 100);
+        }
+        if(deger == 4){
+            CONST.hastalikTaniOran = (CONST.hastalikTaniOran + 50);
+        }
+    }
+
+    public static void siddetliCevap(int deger){
+        if(deger == 0){
+            CONST.hastalikTaniOran = (CONST.hastalikTaniOran + 0);
+        }
+        if(deger == 1){
+            CONST.hastalikTaniOran = (CONST.hastalikTaniOran + 25);
+        }
+        if(deger == 2){
+            CONST.hastalikTaniOran = (CONST.hastalikTaniOran + 50);
+        }
+        if(deger == 3){
+            CONST.hastalikTaniOran = (CONST.hastalikTaniOran + 75);
+        }
+        if(deger == 4){
+            CONST.hastalikTaniOran = (CONST.hastalikTaniOran + 100);
+        }
+    }
+
+    public static void koronaTaniOrani(int deger1,int deger2,int deger3,int deger4,int deger5,int deger6,int deger7,int deger8,int deger9,int deger10,int deger11,int deger12){
+
+        sikCevap(deger1);
+        sikCevap(deger2);
+        sikCevap(deger3);
+        siddetliCevap(deger4);
+        sikCevap(deger5);
+        bazenCevap(deger6);
+        yokCevap(deger7);
+        bazenCevap(deger8);
+        yokCevap(deger9);
+        yokCevap(deger10);
+        bazenCevap(deger11);
+        bazenCevap(deger12);
+    }
+
+    public static void sogukAlginligiTaniOrani(int deger1,int deger2,int deger3,int deger4,int deger5,int deger6,int deger7,int deger8,int deger9,int deger10,int deger11,int deger12){
+        nadirenCevap(deger1);
+        bazenCevap(deger2);
+        yokCevap(deger3);
+        yokCevap(deger4);
+        yokCevap(deger5);
+        nadirenCevap(deger6);
+        sikCevap(deger7);
+        yokCevap(deger8);
+        sikCevap(deger9);
+        yokCevap(deger10);
+        sikCevap(deger11);
+        yokCevap(deger12);
+    }
+
+    public static void gripTaniOrani(int deger1, int deger2, int deger3, int deger4, int deger5, int deger6, int deger7, int deger8, int deger9, int deger10, int deger11, int deger12){
+        sikCevap(deger1);
+        sikCevap(deger2);
+        sikCevap(deger3);
+        yokCevap(deger4);
+        bazenCevap(deger5);
+        sikCevap(deger6);
+        bazenCevap(deger7);
+        bazenCevap(deger8);
+        bazenCevap(deger9);
+        yokCevap(deger10);
+        bazenCevap(deger11);
+        nadirenCevap(deger11);
+    }
+
+    public static void alerjiTaniOrani(int deger1,int deger2,int deger3,int deger4,int deger5,int deger6,int deger7,int deger8,int deger9,int deger10,int deger11,int deger12){
+        yokCevap(deger1);
+        bazenCevap(deger2);
+        bazenCevap(deger3);
+        bazenCevap(deger4);
+        bazenCevap(deger5);
+        yokCevap(deger6);
+        sikCevap(deger7);
+        sikCevap(deger8);
+        sikCevap(deger9);
+        sikCevap(deger10);
+        yokCevap(deger11);
+        yokCevap(deger12);
+    }
+
+    public static void hastalikTani(float deger){
+
+        if(deger > CONST.sogukAlginligiYuzde && deger > CONST.gripYuzde && deger > CONST.alerjiYuzde){
+            CONST.koronaBool = true;
+        }
+        if(deger > CONST.koronaYuzde && deger > CONST.gripYuzde && deger > CONST.alerjiYuzde){
+            CONST.sogukAlginligiBool = true;
+        }
+        if(deger > CONST.koronaYuzde && deger > CONST.sogukAlginligiYuzde && deger > CONST.alerjiYuzde){
+            CONST.gripBool = true;
+        }
+        if(deger > CONST.koronaYuzde && deger > CONST.sogukAlginligiYuzde && deger > CONST.gripYuzde){
+            CONST.alerjiBool = true;
+        }
+    }
+}
